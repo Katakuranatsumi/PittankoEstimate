ee0d39d4d6d70fcdb4185030d165e3f2ca08b1e5
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-04-16 23:23:07 +0900

    トップページをタスク見積もり一覧に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'estimation_of_tasks#index'
   resources :estimation_of_tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
