f846b7ad3d876742e0434b0fa88e3c0d240b66dd
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-04-20 23:46:38 +0900

    入力フォームのタスク種類をセレクトボックスに変更する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def translated_task_type(task_type)
+    t("enums.estimation_of_task.task_type.#{task_type}")
+  end
+
+  def candidated_task_types
+    EstimationOfTask.task_types.keys.map { |k| [translated_task_type(k), k] }
+  end
 end
