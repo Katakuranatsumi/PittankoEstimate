e975c967304bf362a67d5e266f57dbcfa3dcf2a9
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-05-31 17:52:40 +0900

    Sidekiqのダッシュボードを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+
   root to: 'estimation_of_tasks#index'
   resources :estimation_of_tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
 end
